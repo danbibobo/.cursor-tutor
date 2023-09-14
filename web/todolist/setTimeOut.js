ae0e7fa2b2299e857bb13247332c789fc0d834b8
danbibobo <minhanh5665@gmail.com>
2023-09-15 06:22:12 +0700

    new assignment

---
+++ web/todolist/setTimeOut.js
@@ -0,0 +1,16 @@
+// global var
+var second = 0;
+var clear = null;
+
+function timer(){
+    second++;
+    let secs = second %60;
+    let mins = Math.floor(second/60);
+    console.log(secs);
+    console.log(mins);
+}
+function start(){
+    
+}
+
+
